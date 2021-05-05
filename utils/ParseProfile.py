0bf310af26d9dd980cd0ffa3a3b11b1b5eb7ce9c
ryoichi-flect <ryoichi.tachibana@flect.co.jp>
2021-05-05 11:41:54 +0900

    v0.03 create xml with maulty sheets

--- utils/ParseProfile.py
+++ utils/ParseProfile.py
@@ -5,13 +5,7 @@ from openpyxl import Workbook
 
 from utils import utils
 
-def getXmlDataByList(configObj):
-    path = configObj['inputdir']
-    targetNote = configObj['targetNote']
-    targetSubNotes = configObj['targetSubNotes']
-    withns = configObj['withns']
-    sfdc_metadata = configObj['sfdc_metadata']
-
+def getXmlDataByList(path, targetNote, targetSubNotes, withns, sfdc_metadata):
     resultData = []
     for fl in glob.glob(path + "/**/*", recursive=True):
         if withns:
@@ -23,11 +17,11 @@ def getXmlDataByList(configObj):
     # print('getXmlDataByList resultData : ', resultData)
     return resultData
 
-def getXmlDataToCsvByMatrix(configObj, targetSubNotekey):
-    xmlDataList = getXmlDataByList(configObj)
+def getXmlDataToCsvByMatrix(path, targetNote, targetSubNotes, withns, sfdc_metadata, targetSubNotekey):
+    xmldata = getXmlDataByList(path, targetNote, targetSubNotes, withns, sfdc_metadata)
     fileDataMap = {}
 
-    for xmlDataMap in xmlDataList:
+    for xmlDataMap in xmldata:
         print('xmlDataMap:', xmlDataMap)
         index = 0
         cellKey = ''
@@ -60,11 +54,14 @@ def getXmlDataToCsvByMatrix(configObj, targetSubNotekey):
 
 def outputXmlDataToCsvByList(configObj, isOutputFile):
     path = configObj['inputdir']
+    sfdc_metadata = configObj['sfdc_metadata']
+    withns = configObj['withns']
+
     targetNote = configObj['targetNote']
     targetSubNotes = configObj['targetSubNotes']
-    outputFileName = configObj['outputFileName'] + '_' + path.split('/')[- 1] + '_' + targetNote + '.csv'
 
-    xmldata = getXmlDataByList(configObj)
+    outputFileName = configObj['outputFileName'] + '_' + path.split('/')[- 1] + '_' + targetNote + '.csv'
+    xmldata = getXmlDataByList(path, targetNote, targetSubNotes, withns, sfdc_metadata)
 
     if isOutputFile:
         if os.path.exists(outputFileName):
@@ -73,23 +70,33 @@ def outputXmlDataToCsvByList(configObj, isOutputFile):
 
 def outputXmlDataToCsvByMatrix(configObj, isOutputFile):
     path = configObj['inputdir']
+    sfdc_metadata = configObj['sfdc_metadata']
     targetNote = configObj['targetNote']
-    outputFileName = configObj['outputFileName'] + '_' + path.split('/')[- 1] + '_' + targetNote + '.csv'
+    targetSubNotes = configObj['targetSubNotes']
+    withns = configObj['withns']
     targetKey = configObj['targetKey']
 
-    resultMap = getXmlDataToCsvByMatrix(configObj, targetKey)
+    outputFileName = configObj['outputFileName'] + '_' + path.split('/')[- 1] + '_' + targetNote + '.csv'
+
+    dataMap = getXmlDataToCsvByMatrix(path, targetNote, targetSubNotes, withns, sfdc_metadata, targetKey)
     if isOutputFile:
         if os.path.exists(outputFileName):
             os.remove(outputFileName)
-        utils.savetoCSV(resultMap['head'].split(','), resultMap['datas'], outputFileName)
+        utils.savetoCSV(dataMap['head'].split(','), dataMap['datas'], outputFileName)
 
 def outputXmlDataToExcelByMatrix(configObj, isOutputFile):
+    path = configObj['inputdir']
+    sfdc_metadata = configObj['sfdc_metadata']
+    targetNote = configObj['targetNote']
+    targetSubNotes = configObj['targetSubNotes']
+    withns = configObj['withns']
     targetKey = configObj['targetKey']
+
     targetObjs = configObj['targetObjs']
     targetObjs.insert(0, 'filename')
     # print('targetObjs:', targetObjs)
 
-    dataMap = getXmlDataToCsvByMatrix(configObj, targetKey)
+    dataMap = getXmlDataToCsvByMatrix(path, targetNote, targetSubNotes, withns, sfdc_metadata, targetKey)
     # print(dataMap)
     datas = dataMap['datas']
     # print('list(datas):', datas)
@@ -98,7 +105,7 @@ def outputXmlDataToExcelByMatrix(configObj, isOutputFile):
     sheet = wb.active
 
     for i, objName in enumerate(targetObjs, start=0):
-        sheet.cell(1, i+1, value=objName)
+        sheet.cell(i+1, 1, value=objName)
 
     for rowNum, datas in enumerate(datas, start=2):
         print('datas', datas)
@@ -109,7 +116,7 @@ def outputXmlDataToExcelByMatrix(configObj, isOutputFile):
             else:
                 print(rowNum, columnNum, 'none')
                 cellValue = ''
-            sheet.cell(row=rowNum, column=columnNum).value = cellValue
+            sheet.cell(row=columnNum, column=rowNum).value = cellValue
 
     wb.save('testリスト追加.xlsx')
 
